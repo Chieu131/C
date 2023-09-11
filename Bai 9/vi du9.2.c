f80d9121e64c1151eccebcd59ea433328a5f1929
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2023-09-11 15:14:38 +0700

    Create vi du9.2.c

---
+++ Bai 9/vi du9.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("The even numbers from 1 to 25 are : \n\n");
+    for (int num3; num <=25 ; num+=3) {
+        printf("%d\n",num);
+
+    }
+}
