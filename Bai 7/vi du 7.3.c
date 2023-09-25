3807eab393f49a6e6658e5445ee1491f6b70cfda
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2023-09-25 11:39:37 +0700

    Delete Bai 7 directory

--- Bai 7/vi du 7.3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- int main() {
-     int year;
-     printf("\nPlease enter a year:");
-     scanf("%d", &year);
-     if (year % 4 == 0 && year % 100 != 0 || year % 400  == 0) {
-         printf("\nd is a leap year!", year);
-     }
- }
