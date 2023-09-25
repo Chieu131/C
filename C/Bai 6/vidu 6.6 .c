2624c491fab8ddb0b1ffe8a8ad7e6ecb40168979
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2023-09-25 11:41:50 +0700

    Create vidu 6.6 .c

---
+++ C/Bai 6/vidu 6.6 .c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+int main(){printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\aaaaaaaaaaaaa");
+}
